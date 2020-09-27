f22e1c4671b19b14d775e83006016388410e9104
Devlopper Fahmi <coursangularfahmi@gmail.com>
2020-09-27 16:16:58 +0100

    Accessing Protected API Resources

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AuthHttp } from 'angular2-jwt';
+import { map } from "rxjs/operators";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor(private authHttp:AuthHttp) { }
+
+  getOrders(){
+    return this.authHttp.get('/api/orders')
+    .pipe(
+      map(response => response.json()));
+  }
 }
